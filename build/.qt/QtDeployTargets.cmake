64a0f06f4cc678bdc1950e32cdd1ae32563b6c8f
Jeruin <JeruinpaulinoRosa.dev@gmail.com>
2025-12-18 17:38:43 -0400

    eliminando build

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appReloj_FILE /home/yeru/Documentos/Dev/reloj-qt6/build/appReloj)
-set(__QT_DEPLOY_TARGET_appReloj_TYPE EXECUTABLE)
--- build/.qt/appReloj_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appReloj")
-set(working_dir "/home/yeru/Documentos/Dev/reloj-qt6")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/yeru/Documentos/Dev/reloj-qt6/build/.qt/appReloj_res.txt")
